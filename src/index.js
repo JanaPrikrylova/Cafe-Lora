ac3a85b7777c725710f151704318596e275bc3c6
JanaPrikrylova <prikrylovajana003@gmail.com>
2021-07-09 11:14:16 +0200

    Zprovoznění navigace

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './style.css';
 
 console.log('funguju!');
+
+// -------------- Zprovoznění navigace -------------
+
+const btnElm = document.querySelector('#nav-btn');
+const navElm = document.querySelector('nav');
+
+btnElm.addEventListener('click', () => {
+  navElm.classList.toggle('nav-closed');
+})
+
+const aElm = document.querySelectorAll('a');
+
+const navigation = () => {
+  const navElm = document.querySelector('nav');
+  navElm.classList.toggle('nav-closed');
+}
+
+for (let i = 0; i < aElm.length; i++) {
+  aElm[i].addEventListener('click', navigation);
+}
