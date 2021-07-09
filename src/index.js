0808781f2ab1b3b054fcd105eab890bb52e2f74a
JanaPrikrylova <prikrylovajana003@gmail.com>
2021-07-09 11:36:07 +0200

    Zprovoznění objednávání

--- src/index.js
+++ src/index.js
@@ -21,3 +21,23 @@ const navigation = () => {
 for (let i = 0; i < aElm.length; i++) {
   aElm[i].addEventListener('click', navigation);
 }
+
+// ------------- Objednávání -----------------
+
+const orderBtnElm = document.querySelector('.order-btn');
+const drinkCupElm = document.querySelector('.drink__cup');
+let ordered = false;
+
+const order = () => {
+  if (ordered === true) {
+  orderBtnElm.textContent = "Zrušit";
+  drinkCupElm.classList.toggle = "drink__cup--selected";
+  ordered = false;
+  } else {
+  orderBtnElm.textContent = "Objednat";
+  drinkCupElm.classList.toggle = "drink__cup--selected";
+  ordered = true;
+  }
+}
+
+orderBtnElm.addEventListener('click', order);
